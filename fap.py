6786ff6956aaa68b905244660e76e11c47aa603a
pongsakorn-kunna <alekatoza@hotmail.com>
2017-03-31 16:07:06 +0700

    fap

--- fap.py
+++ fap.py
@@ -2,6 +2,7 @@ def main():
     a = int(input())
     b = int(input())
     x = a+b;
+    print(x)
     
 if __name__ == "__main__":
     main()
---
+++ fappy.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3 
+# @Author: pongsakorn-kunna
+# @Date:   2017-03-31 15:56:06
+# @Last Modified time: 2017-03-31 15:56:06
